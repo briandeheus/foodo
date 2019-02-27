d00093cb16bb0ac6eec5009bc3549ad8a9ca2045
Brian de Heus <me@brian.jp>
2019-02-27 22:27:30 +0900

    Configuration overload

--- build-event-worker.sh
+++ build-event-worker.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-build-api-server.sh $1
-
 docker build -f Dockerfile.event-worker --tag briandeheus/event-worker:$1 .
-
 docker push briandeheus/event-worker:$1
-docker push briandeheus/socket-server:$1
